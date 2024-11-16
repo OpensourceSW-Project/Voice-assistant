abb778c94276fe191a4f8e013de487a11dfee4d6
Yjongbeom <yukjb5154@naver.com>
2024-11-16 21:11:29 +0900

    post 파라미터에서 data.get으로 변경

--- apps/views.py
+++ apps/views.py
@@ -5,6 +5,7 @@ from rest_framework.response import Response
 
 from apps.serializers import ReservationSerializer, AccommodationSerializer
 from apps.models import Reservation, Accommodation, User
+from datetime import datetime
 
 
 # 숙소 정보 조회 GET(완)
@@ -39,7 +40,45 @@ from apps.models import Reservation, Accommodation, User
 #
 #     # 유저 예약 생성
 #     def post(self, request):
-#         pass
+#         user_id = request.data.get('user_id')
+#         accommodation_name = request.data.get('accommodation_name')
+#         check_in_date = request.data.get('check_in_date')
+#         check_out_date = request.data.get('check_out_date')
+#         price = request.data.get('price', 0)
+#
+#         if not user_id or not accommodation_name or not check_in_date or not check_out_date:
+#             return Response({"error": "user_id, accommodation_name, check_in_date, check_out_date required"},
+#                             status=status.HTTP_400_BAD_REQUEST)
+#
+#         try:
+#             user = User.objects.get(id=user_id)
+#             accommodation = Accommodation.objects.get(name=accommodation_name)
+#         except User.DoesNotExist:
+#             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+#         except Accommodation.DoesNotExist:
+#             return Response({"error": "Accommodation not found"}, status=status.HTTP_404_NOT_FOUND)
+#
+#             # 예약 생성
+#         try:
+#             reservation = Reservation.objects.create(
+#                 user=user,
+#                 accomodation=accommodation,
+#                 check_in_date=check_in_date,
+#                 check_out_date=check_out_date,
+#                 status=True,
+#                 price=price,
+#                 create_at=datetime.now().date(),
+#                 updated_at=datetime.now().date(),
+#             )
+#
+#             serializer = ReservationSerializer(reservation)
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#         except Exception as e:
+#             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+
 
 class AccommodationInfo(APIView):
     # 숙소 정보 조회
@@ -84,8 +123,8 @@ class LikeAccommodation(APIView):
 
     # 유저 좋아요 생성
     def post(self, request, format=None):
-        user_id = request.query_params.get('user_id')
-        accommodation_name = request.query_params.get('accommodation_name')
+        user_id = request.data.get('user_id')
+        accommodation_name = request.data.get('accommodation_name')
 
         if not user_id or not accommodation_name: # user_id과 accommodation_name을 query_params에 안넣었을 때
             return Response({"error": "user_id accommodation_name required"}, status=status.HTTP_400_BAD_REQUEST)
@@ -103,8 +142,8 @@ class LikeAccommodation(APIView):
 
     # 유저 좋아요 삭제
     def delete(self, request, format=None):
-        user_id = request.query_params.get('user_id')
-        accommodation_name = request.query_params.get('accommodation_name')
+        user_id = request.data.get('user_id')
+        accommodation_name = request.data.get('accommodation_name')
 
         if not user_id or not accommodation_name: # user_id과 accommodation_name을 query_params에 안넣었을 때
             return Response({"error": "user_id accommodation_name required"}, status=status.HTTP_400_BAD_REQUEST)
